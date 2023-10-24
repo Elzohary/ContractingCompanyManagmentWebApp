f0863b327f410eb8cbc912c6b50ca3178372655e
Mohamed Elzohary <96190296+Elzohary@users.noreply.github.com>
2023-10-24 18:34:47 +0300

    header-sidenav-v1

--- src/app/public-company-profie/Shared/shared.module.ts
+++ src/app/public-company-profie/Shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { HeaderComponent } from './Components/header/header.component';
 
@@ -16,6 +17,7 @@ import {MatButtonModule} from '@angular/material/button';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
