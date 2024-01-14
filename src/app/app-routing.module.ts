f5d6c11da045b1cfba1db75a0bc585fad9a21a4d
Mohamed Elerian <103853528+Elerian07@users.noreply.github.com>
2024-01-14 03:17:54 +0300

    MenuBtnFix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public-company-profie/Home/Home.component';
 import { AboutUsComponent } from './public-company-profie/about-us/Components/about-us/about-us.component';
+import { PublicCompanyProfileComponent } from './public-company-profie/public-company-profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: PublicCompanyProfileComponent },
   { path: 'about-us', component: AboutUsComponent }
 ];
 
