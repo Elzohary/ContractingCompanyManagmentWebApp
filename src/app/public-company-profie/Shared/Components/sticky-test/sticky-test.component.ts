83f97fa99e036fd5346888918449eb93035ec85b
Mohamed Elerian <103853528+Elerian07@users.noreply.github.com>
2024-01-16 03:52:00 +0300

    Contact Section

--- src/app/public-company-profie/Shared/Components/sticky-test/sticky-test.component.ts
+++ src/app/public-company-profie/Shared/Components/sticky-test/sticky-test.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { StickyCard } from 'src/app/public-company-profie/Models/sticky-card';
 
 @Component({
   selector: 'app-sticky-test',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./sticky-test.component.scss']
 })
 export class StickyTestComponent {
-
+  
 }
